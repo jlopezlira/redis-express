7849e876e1a3383a5a8120b3ab5b5f8170f37d38
Juan José López Lira <jlopezlira@gmail.com>
2023-08-23 19:51:24 -0500

    fix(index.js): fix unused variable warning for axios import feat(index.js): add redis support for caching API responses to improve performance and reduce external API calls feat(index.js): add route to fetch users from remote API and cache the response in redis feat(index.js): add route to fetch user by id from remote API and cache the response in redis

--- src/index.js
+++ src/index.js
@@ -1,31 +1,78 @@
 import axios from "axios";
 import express from "express";
+import redis from "redis";
 import responseTime from "response-time";
+
+const API_URL = "https://jsonplaceholder.typicode.com";
+
 const app = express();
+const client = redis.createClient({
+  host: "127.0.0.1",
+  port: 6379,
+});
 
 app.use(
-  responseTime((req, res, time) => {
+  responseTime((req, _, time) => {
     console.log(`Request to ${req.url} took ${time}ms`);
   })
 );
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Main route
+app.get("/", (_, res) => {
+  res.send("Hello Redis on Express!");
 });
 
-app.get("/users", (req, res) => {
-  axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
-    res.send(response.data);
+// Users route
+app.get("/users", async (_, res) => {
+  // Check the redis store for the data first
+  client.get("users", (err, result) => {
+    if (err) res.send(err);
+    if (result) {
+      res.send(JSON.parse(result));
+    } else {
+      next();
+    }
   });
-});
 
-app.get("/users/:id", (req, res) => {
-  const id = req.params.id;
-  axios
-    .get(`https://jsonplaceholder.typicode.com/users/${id}`)
-    .then((response) => {
+  // Fetch directly from remote api if no cache found
+  try {
+    const response = await axios.get(`${API_URL}/users`);
+    // Save the API response in Redis store, data expire time in 3600 seconds, it means one hour
+    client.set("users", 3600, JSON.stringify(response.data), (err) => {
+      if (err) res.send(err);
+      // Send JSON response to client
       res.send(response.data);
     });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+
+  // Check the redis store for the data first
+  client.get(`user${id}`, (err, result) => {
+    if (err) res.send(err);
+    if (result) {
+      res.send(JSON.parse(result));
+    } else {
+      next();
+    }
+  });
+
+  // Fetch directly from remote api if no cache found
+  try {
+    const response = await axios.get(`${API_URL}/${id}`);
+    // Save the API response in Redis store, data expire time in 3600 seconds, it means one hour
+    client.set(`user${id}`, 3600, JSON.stringify(response.data), (err) => {
+      if (err) res.send(err);
+      // Send JSON response to client
+      res.send(response.data);
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
 });
 
 app.listen(3000, () => {
